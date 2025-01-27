768cdc416e4bcd17e76a8bab85a1fca79aaa8777
Alan Farias <aaof.alan@gmail.com>
2025-01-27 17:55:33 -0300

    (Em desenvolvimento) Modal Recuperação de Senha

---
+++ frontend/src/Modal/RecuperarSenha.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const RecuperarSenha = () => {
+  return (
+    <div>
+      <h1>Recuperar Senha</h1>
+      <p>Página em construção.</p>
+    </div>
+  );
+};
+
+export default RecuperarSenha;
