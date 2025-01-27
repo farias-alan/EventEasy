1c73233431c66917b03de45f9fcb5734e461cc0e
Alan Farias <aaof.alan@gmail.com>
2025-01-27 17:50:58 -0300

    feat: Navegação das rotas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import HomePage from './pages/HomePage/HomePage'; 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage/HomePage';
+import LoginPage from './pages/LoginPage/LoginPage';
+import RecuperarSenha from './Modal/RecuperarSenha'; 
+import CadastroPage from './pages/CadastroPage/CadastroPage';
 
 function App() {
   return (
-    <div>
-      <HomePage />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/recuperar-senha" element={<RecuperarSenha />} />
+        <Route path="/cadastro" element={<CadastroPage />} />
+      </Routes>
+    </Router>
   );
 }
 
