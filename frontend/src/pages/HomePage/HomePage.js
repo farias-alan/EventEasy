0f7da21b8c66c617df062f1e56d722fd385731db
Alan Farias <aaof.alan@gmail.com>
2025-01-27 17:49:11 -0300

    feat: Funcionalidade dos Botões - Página Principal

--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -1,24 +1,36 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./HomePage.css";
 import logo from "../../templates/logo.png";
 
 const HomePage = () => {
+  const navigate = useNavigate();
+
+  const handleLoginClick = () => {
+    navigate("/login"); 
+  };
+
+  const handleRegisterClick = () => {
+    navigate("/cadastro"); 
+  };
+
   return (
     <div className="home-container">
-      
       <img src={logo} alt="EventEasy Logo" className="logo" />
-
-     
       <div className="content">
         <h2 className="description">
           EventEasy, sua<br />
-          plataforma de gerenciar<br />
-          eventos.
+          plataforma de<br />
+          gerenciar eventos.
         </h2>
         <div className="buttons">
-          <button className="btn btn-primary">Entrar</button>
+          <button className="btn btn-primary" onClick={handleLoginClick}>
+            Entrar
+          </button>
           <p>OU</p>
-          <button className="btn btn-secondary">Cadastrar</button>
+          <button className="btn btn-secondary" onClick={handleRegisterClick}>
+            Cadastrar
+          </button>
         </div>
       </div>
     </div>
